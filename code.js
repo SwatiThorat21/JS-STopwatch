8ab985a0b7ed408fc72d03a52cbcc1c0d9697985
Swati <swatic019@gmail.com>
2022-06-19 23:09:31 +0530

    Updated

--- code.js
+++ code.js
@@ -7,27 +7,49 @@ let startbtn = document.getElementById('startbtn');
 let stopbtn = document.getElementById('stopbtn');
 let resetbtn = document.getElementById('resetbtn');
 
-startbtn.addEventListener('click',startbtn_click);
-function startbtn_click(){
-timer = true;
-stopwatch();
-
-};
-
-stopbtn.addEventListener('click',stopbtn_click);
-function stopbtn_click(){
-    timer = false;
-
-};
-
-resetbtn.addEventListener('click',resetbtn_click);
-function resetbtn_click(){
-    timer = false;
+startbtn.addEventListener('click', startbtn_click);
+function startbtn_click() {
+    timer = true;
+    stopWatch();
     
+
 };
 
-function stopwatch(){
+stopbtn.addEventListener('click', stopbtn_click);
+function stopbtn_click() {
+    timer = false;
 
-    setTimeout("stopwatch()", 1000);
+};
+
+resetbtn.addEventListener('click', resetbtn_click);
+function resetbtn_click() {
+    timer = false;
+    hr = 0;
+    min = 0;
+    sec = 0;
+    
+
+};
+
+function stopWatch() {
+    if (timer == true) {
+        sec = sec + 1;
+        if (sec == 60) {
+            min = min + 1;
+            sec = 0;
+        }
+        if (min == 60) {
+            hr = hr + 1;
+            min = 0;
+            sec = 0;
+
+        }
+        document.getElementById('hr').innerHTML = hr;
+        document.getElementById('min').innerHTML = min;
+        document.getElementById('sec').innerHTML = sec;
+
+        setTimeout("stopWatch()", 1000);
+    }
 
 }
+
